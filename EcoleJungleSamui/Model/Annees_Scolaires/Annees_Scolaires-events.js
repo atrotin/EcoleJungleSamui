91c470a8335776c8563b4b91b378fc76bc93762e
atrotin <arnaud.trotin@gmail.com>
2014-08-23 00:44:56 +0000

    Delete Annees_Scolaires-events.js

--- EcoleJungleSamui/Model/Annees_Scolaires/Annees_Scolaires-events.js
+++
@@ -1,1 +0,0 @@
-﻿model.Annees_Scolaires.events.onInit = function() {	this.UID_Creation = currentUser().fullName;	this.Date_Creation = new Date();};model.Annees_Scolaires.events.onSave = function() {	this.UID_MAJ = currentUser().fullName;	this.Derniere_MAJ = new Date();};
